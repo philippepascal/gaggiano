235386b70fa9999626f7aa75ea5e9b884ac5cffd
philippepascal <philippe@pascalonline.org>
2025-02-14 00:48:24 -0800

    starting to tinker with new tab

---
+++ gaggiano-v2/lv_buildUI.h
@@ -0,0 +1,42 @@
+/**
+ * @file lv_buildUI.h
+ *
+ */
+
+#ifndef LV_DEMO_WIDGETS_H
+#define LV_DEMO_WIDGETS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+//#include "../lv_demos.h"
+//adding arduino for using serial.print
+#include "Arduino.h"
+#include <lvgl.h>
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lv_demo_widgets(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_DEMO_WIDGETS_H*/
---
+++ gaggiano-v2/my_logging.cpp
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+extern "C" {
+  #include "my_logging.h"
+}
+
+void my_log(const char *msg) {
+  Serial.println(msg);
+  Serial.flush();
+}
